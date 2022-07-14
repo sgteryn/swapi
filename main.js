0a76cec0732aa5b2fd068b6a2ad51b823da207ab
sgteryn <sgteryn@gmail.com>
2022-07-13 23:54:28 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+const button = document.querySelector('#grbutton')
+
+
+function buttonClicked(evt) {
+    console.log('Button Clicked')
+}
+
+
+button.addEventListener('click', buttonClicked)
